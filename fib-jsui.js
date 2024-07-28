e3923ed7de14668d2e305c5e8eafad9843fe4cff
Zack Steinkamp <zack@steinkamp.us>
2024-07-28 14:26:59 -0700

    highlight note playing

--- fib-jsui.js
+++ fib-jsui.js
@@ -1,18 +1,22 @@
-outlets=1;
-autowatch=1;
+outlets = 1;
+autowatch = 1;
 
 sketch.default2d();
 sketch.glloadidentity();
-sketch.glortho(0., 1, -0.5, 0.5, -1,1.);
+sketch.glortho(0, 1, -0.5, 0.5, -1, 1);
 var pattern = [];
 var utils = {
-  scale: function(array, newMin, newMax) {
+  scale: function (array, newMin, newMax) {
     // get range
-    var min = null
+    var min = null;
     var max = null;
     for (var i = 0; i < array.length; i++) {
-      if (min === null || array[i] < min) { min = array[i]; }
-      if (max === null || array[i] > max) { max = array[i]; }
+      if (min === null || array[i] < min) {
+        min = array[i];
+      }
+      if (max === null || array[i] > max) {
+        max = array[i];
+      }
     }
     var range = max - min;
 
@@ -20,7 +24,7 @@ var utils = {
 
     var coeff = range ? newRange / parseFloat(range) : 0.0;
 
-    var offset = newMin - (min * coeff);
+    var offset = newMin - min * coeff;
 
     var returnArray = [];
     for (var i = 0; i < array.length; i++) {
@@ -39,81 +43,115 @@ var utils = {
 
     return returnArray;
   },
-  HSLToRGB: function(h,s,l) {
+  HSLToRGB: function (h, s, l) {
     //exports.log({ h: h, s: s, l: l });
 
     var c = (1 - Math.abs(2 * l - 1)) * s,
-        x = c * (1 - Math.abs((h / 60) % 2 - 1)),
-        m = l - c/2,
-        r = 0,
-        g = 0,
-        b = 0;
+      x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+      m = l - c / 2,
+      r = 0,
+      g = 0,
+      b = 0;
     if (0 <= h && h < 60) {
-      r = c; g = x; b = 0;
+      r = c;
+      g = x;
+      b = 0;
     } else if (60 <= h && h < 120) {
-      r = x; g = c; b = 0;
+      r = x;
+      g = c;
+      b = 0;
     } else if (120 <= h && h < 180) {
-      r = 0; g = c; b = x;
+      r = 0;
+      g = c;
+      b = x;
     } else if (180 <= h && h < 240) {
-      r = 0; g = x; b = c;
+      r = 0;
+      g = x;
+      b = c;
     } else if (240 <= h && h < 300) {
-      r = x; g = 0; b = c;
+      r = x;
+      g = 0;
+      b = c;
     } else if (300 <= h && h < 360) {
-      r = c; g = 0; b = x;
+      r = c;
+      g = 0;
+      b = x;
     }
     return {
       r: r + m,
       g: g + m,
-      b: b + m
+      b: b + m,
     };
   },
-  log: function() {
-    for(var i=0,len=arguments.length; i<len; i++) {
+  log: function () {
+    for (var i = 0, len = arguments.length; i < len; i++) {
       var message = arguments[i];
-      if(message && message.toString) {
+      if (message && message.toString) {
         var s = message.toString();
-        if(s.indexOf("[object ") >= 0) {
+        if (s.indexOf("[object ") >= 0) {
           s = JSON.stringify(message);
         }
         post(s);
-      }
-      else if(message === null) {
+      } else if (message === null) {
         post("<null>");
-      }
-      else {
+      } else {
         post(message);
       }
     }
     post("\n");
-  }
+  },
 };
 
 var OUTLET_DURATION = 0;
 
-function draw()
-{
+var flashIdx = null;
+
+function flash(idx) {
+  flashIdx = idx;
+  draw();
+  refresh();
+  var t = new Task(function () {
+    flashIdx = null;
+    draw();
+    refresh();
+  });
+  t.schedule(250);
+}
+
+function draw() {
   sketch.glclearcolor(1.0, 1.0, 1.0, 0);
   sketch.glclear();
 
   var tap;
   var color;
 
-  var offsets = utils.scale(pattern.map( function(tap) { return tap.time_offset; } ), -3, 4);
+  var offsets = utils.scale(
+    pattern.map(function (tap) {
+      return tap.time_offset;
+    }),
+    -3,
+    4
+  );
 
   for (var i = 0; i < pattern.length; i++) {
     tap = pattern[i];
     sketch.moveto(offsets[i], 0.05);
     // set foreground color
-    hue = (360 + (30 * tap.note_incr) % 360) % 360;
+    hue = (360 + ((30 * tap.note_incr) % 360)) % 360;
     //utils.log(tap.note_incr);
     //utils.log(hue);
 
-    // outer black circle
-    sketch.glcolor(0, 0, 0, 1);
-    sketch.circle(0.28 * tap.velocity_coeff);
+    var circleBorder = 0;
+    if (flashIdx === i.toString()) {
+      circleBorder = 1;
+    }
+    // outer circle
+    //utils.log("idx: " + i + "  circleBorder: " + circleBorder);
+    sketch.glcolor(circleBorder, circleBorder, circleBorder, 1);
+    sketch.circle(0.28 * tap.velocity_coeff, 4);
 
     // inner colored circle
-    color = utils.HSLToRGB(hue, .75, .60);
+    color = utils.HSLToRGB(hue, 0.75, 0.6);
     sketch.glcolor(color.r, color.g, color.b, 1);
     sketch.circle(0.25 * tap.velocity_coeff);
   }
@@ -121,8 +159,8 @@ function draw()
 
   if (pattern.length > 0) {
     sketch.textalign("center");
-    sketch.glcolor(1,1,1,1);
-    outlet(OUTLET_DURATION, parseInt(pattern[pattern.length - 1].time_offset)/1000.0);
+    sketch.glcolor(1, 1, 1, 1);
+    outlet(OUTLET_DURATION, parseInt(pattern[pattern.length - 1].time_offset) / 1000.0);
   }
 }
 
@@ -134,27 +172,24 @@ function update(data) {
   refresh();
 }
 
-function onresize(w,h)
-{
-	log(w,h);
-	draw();
-	refresh();
+function onresize(w, h) {
+  log(w, h);
+  draw();
+  refresh();
 }
 onresize.local = 1; //private
 
-function forcesize(w,h)
-{
-  if (w!=h*8) {
-    h = Math.floor(w/8);
-    w = h*8;
-    box.size(w,h);
+function forcesize(w, h) {
+  if (w != h * 8) {
+    h = Math.floor(w / 8);
+    w = h * 8;
+    box.size(w, h);
   }
 }
 forcesize.local = 1; //private
 
-function onresize(w,h)
-{
-  forcesize(w,h);
+function onresize(w, h) {
+  forcesize(w, h);
   draw();
   refresh();
 }
--- fibonacciNoteEcho.js
+++ fibonacciNoteEcho.js
@@ -1,51 +1,49 @@
-inlets=9
-outlets=4
+inlets = 9;
+outlets = 4;
 
 var utils = {
-  log: function() {
-    for(var i=0,len=arguments.length; i<len; i++) {
+  log: function () {
+    for (var i = 0, len = arguments.length; i < len; i++) {
       var message = arguments[i];
-      if(message && message.toString) {
+      if (message && message.toString) {
         var s = message.toString();
-        if(s.indexOf("[object ") >= 0) {
+        if (s.indexOf("[object ") >= 0) {
           s = JSON.stringify(message);
         }
         post(s);
-      }
-      else if(message === null) {
+      } else if (message === null) {
         post("<null>");
-      }
-      else {
+      } else {
         post(message);
       }
     }
     post("\n");
-  }
+  },
 };
 
-// inlet index -- used to identify an element in the 
+// inlet index -- used to identify an element in the
 // 'options' array defined below
 var INLET_NOTE = 0;
 var INLET_VELOCITY = 1;
 var INLET_TIME_BASE = 2;
 var INLET_TIMESCALE = 3;
 var INLET_ITERATIONS = 4;
-var INLET_DUR_BASE = 5
+var INLET_DUR_BASE = 5;
 var INLET_DUR_DECAY = 6;
 var INLET_NOTE_INCR = 7;
 var INLET_VELOCITY_DECAY = 8;
 
 // the position in the options array corresponds to the inlet index
 var options = [
-  0,     // INLET_NOTE
-  0,     // INLET_VELOCITY
-  300,   // INLET_TIME_BASE
-  1,     // INLET_TIMESCALE
-  4,     // INLET_ITERATIONS
-  250,   // INLET_DUR_BASE
+  0, // INLET_NOTE
+  0, // INLET_VELOCITY
+  300, // INLET_TIME_BASE
+  1, // INLET_TIMESCALE
+  4, // INLET_ITERATIONS
+  250, // INLET_DUR_BASE
   0.667, // INLET_DUR_DECAY
-  0,     // INLET_NOTE_INCR
-  0.8    // INLET_VELOCITY_DECAY
+  0, // INLET_NOTE_INCR
+  0.8, // INLET_VELOCITY_DECAY
 ];
 
 // outlets
@@ -68,7 +66,7 @@ function setupPattern() {
     note_incr: 0,
     velocity_coeff: 1,
     duration: options[INLET_DUR_BASE],
-    time_offset: 0
+    time_offset: 0,
   });
 
   var prv = 1;
@@ -79,13 +77,13 @@ function setupPattern() {
 
   for (var i = 1; i < options[INLET_ITERATIONS]; i++) {
     //utils.log(fib);
-    new_time_offset = prv_time_offset + options[INLET_TIMESCALE] * options[INLET_TIME_BASE] * fib
+    new_time_offset = prv_time_offset + options[INLET_TIMESCALE] * options[INLET_TIME_BASE] * fib;
 
     pattern.push({
       note_incr: i * options[INLET_NOTE_INCR],
       velocity_coeff: Math.pow(options[INLET_VELOCITY_DECAY], i),
       duration: options[INLET_DUR_BASE] * Math.pow(options[INLET_DUR_DECAY], i),
-      time_offset: new_time_offset
+      time_offset: new_time_offset,
     });
     tmp = fib;
     fib = fib + prv;
@@ -97,17 +95,16 @@ function setupPattern() {
   // Pass 'update' as the head of the array sent to the JSUI outlet calls the
   // 'update' method in the jsui object with the rest of the pattern array as
   // js args. This results in the visualization being redrawn.
-  outlet(OUTLET_JSUI, ['update'].concat(pattern));
+  outlet(OUTLET_JSUI, ["update"].concat(pattern));
 }
-
 
 // Returns a function that when executed will send a note of a given pitch,
 // velocity, and duration to the outlets.
 function makeTask(i, p, n, v) {
-  return function() {
+  return function () {
     n = n + p.note_incr;
     v = parseInt(v * p.velocity_coeff);
-    var d = p.duration
+    var d = p.duration;
 
     //utils.log({
     //  i: i,
@@ -119,7 +116,8 @@ function makeTask(i, p, n, v) {
     outlet(OUTLET_DURATION, d);
     outlet(OUTLET_VELOCITY, v);
     outlet(OUTLET_NOTE, n);
-  }
+    outlet(OUTLET_JSUI, ["flash"].concat(i.toString()));
+  };
 }
 
 function msg_int(value) {
@@ -149,7 +147,7 @@ function handleMessage(value) {
       // Schedule a note-playing task to execute for each element in the
       // pattern, at time_offset in the future.
       // The first element is time_offset === 0.
-      var t = new Task( makeTask(idx, pattern[idx], options[INLET_NOTE], options[INLET_VELOCITY]) );
+      var t = new Task(makeTask(idx, pattern[idx], options[INLET_NOTE], options[INLET_VELOCITY]));
       t.schedule(pattern[idx].time_offset);
     }
   }
