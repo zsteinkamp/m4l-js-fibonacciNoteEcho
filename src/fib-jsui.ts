34e6fc0c872938dc12170fed962de9ec093c74cf
Zack Steinkamp <zack@steinkamp.us>
2024-08-16 23:26:34 -0700

    prettier-eslint

--- src/fib-jsui.ts
+++ src/fib-jsui.ts
@@ -1,68 +1,69 @@
-outlets = 1;
-autowatch = 1;
+outlets = 1
+autowatch = 1
 
-const OUTLET_DUR = 0;
+const OUTLET_DUR = 0
 
-const ASPECT = 400 / 111;
+const ASPECT = 400 / 111
 
-sketch.default2d();
-sketch.glloadidentity();
+sketch.default2d()
+sketch.glloadidentity()
 
-let uiPattern: Step[] = [];
-var utils = {
+let uiPattern: Step[] = []
+const utils = {
   scale: function (array: number[], newMin: number, newMax: number) {
-
     // get range
     let min: number = array[0]
-    let max: number = array[0];
-    for (var i = 1; i < array.length; i++) {
-      if (min === null || array[i] < min) { min = array[i]; }
-      if (max === null || array[i] > max) { max = array[i]; }
+    let max: number = array[0]
+    for (let i = 1; i < array.length; i++) {
+      if (min === null || array[i] < min) {
+        min = array[i]
+      }
+      if (max === null || array[i] > max) {
+        max = array[i]
+      }
     }
-    const range = max - min;
-    const newRange = newMax - newMin;
-    const coeff = range ? newRange / Math.floor(range) : 0.0;
-    const offset = newMin - (min * coeff);
+    const range = max - min
+    const newRange = newMax - newMin
+    const coeff = range ? newRange / Math.floor(range) : 0.0
+    const offset = newMin - min * coeff
 
-    var returnArray = [];
+    const returnArray = []
     for (let i = 0; i < array.length; i++) {
-      returnArray.push(array[i] * coeff + offset);
+      returnArray.push(array[i] * coeff + offset)
     }
 
-    return returnArray;
+    return returnArray
   },
   log: function (_: any) {
-    for (var i = 0, len = arguments.length; i < len; i++) {
-      var message = arguments[i];
+    for (let i = 0, len = arguments.length; i < len; i++) {
+      const message = arguments[i]
       if (message && message.toString) {
-        var s = message.toString();
-        if (s.indexOf("[object ") >= 0) {
-          s = JSON.stringify(message);
+        let s = message.toString()
+        if (s.indexOf('[object ') >= 0) {
+          s = JSON.stringify(message)
         }
-        post(s);
-      }
-      else if (message === null) {
-        post("<null>");
-      }
-      else {
-        post(message);
+        post(s)
+      } else if (message === null) {
+        post('<null>')
+      } else {
+        post(message)
       }
     }
-    post("\n");
-  }
-};
+    post('\n')
+  },
+}
 
 function flash(idx: string) {
-  const flashIdx = parseInt(idx);
+  const flashIdx = parseInt(idx)
   uiPattern[flashIdx].is_on = true
-  draw();
-  refresh();
-  var t = new Task(function () {
+  draw()
+  refresh()
+  const t = new Task(function () {
     uiPattern[flashIdx].is_on = false
-    draw();
-    refresh();
-  });
-  t.schedule(uiPattern[flashIdx].duration);
+    draw()
+    refresh()
+  })
+  t.schedule(uiPattern[flashIdx].duration)
 }
 
 const COLOR_BG = max.getcolor('live_lcd_bg')
@@ -72,42 +73,50 @@ const COLOR_TITLE = max.getcolor('live_lcd_title')
 const colors: Color[] = [
   // generated at https://supercolorpalette.com/?scp=G0-lch-FF6561-F58126-C49C00-84AE04-00B950-00BE93-00BFD5-00BAFF-00AEFF-7397FF-DA79F8-FF5DBC
   // using the 'LCH' color model
-  "#FF6561",
-  "#F58126",
-  "#C49C00",
-  "#84AE04",
-  "#00B950",
-  "#00BE93",
-  "#00BFD5",
-  "#00BAFF",
-  "#00AEFF",
-  "#7397FF",
-  "#DA79F8",
-  "#FF5DBC"
+  '#FF6561',
+  '#F58126',
+  '#C49C00',
+  '#84AE04',
+  '#00B950',
+  '#00BE93',
+  '#00BFD5',
+  '#00BAFF',
+  '#00AEFF',
+  '#7397FF',
+  '#DA79F8',
+  '#FF5DBC',
 ].map((hex: string) => {
-  const matches = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return matches ? [
-    parseInt(matches[1], 16) / 255.0,
-    parseInt(matches[2], 16) / 255.0,
-    parseInt(matches[3], 16) / 255.0,
-    1
-  ] : [0, 0, 0, 1];
+  const matches = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  return matches
+    ? [
+        parseInt(matches[1], 16) / 255.0,
+        parseInt(matches[2], 16) / 255.0,
+        parseInt(matches[3], 16) / 255.0,
+        1,
+      ]
+    : [0, 0, 0, 1]
 })
 
 function draw() {
-  sketch.glclearcolor(COLOR_BG);
-  sketch.glclear();
-  sketch.fontsize(9.5);
+  sketch.glclearcolor(COLOR_BG)
+  sketch.glclear()
+  sketch.fontsize(9.5)
 
-  const offsets = utils.scale(uiPattern.map(function (tap) { return tap.time_offset; }), -(ASPECT * 0.8), ASPECT * .8);
+  const offsets = utils.scale(
+    uiPattern.map(function (tap) {
+      return tap.time_offset
+    }),
+    -(ASPECT * 0.8),
+    ASPECT * 0.8
+  )
 
   //for (var i = uiPattern.length - 1; i >= 0; i--) {
-  for (var i = 0; i < uiPattern.length; i++) {
-    const tap = uiPattern[i];
-    sketch.moveto(offsets[i], 0);
+  for (let i = 0; i < uiPattern.length; i++) {
+    const tap = uiPattern[i]
+    sketch.moveto(offsets[i], 0)
 
     // ability to flash the border
-    var circleBorder = COLOR_LINE;
+    let circleBorder = COLOR_LINE
     if (tap.is_on) {
       circleBorder = COLOR_TITLE
     }
@@ -116,30 +125,30 @@ function draw() {
 
     // outer circle
     //utils.log("idx: " + i + "  circleBorder: " + circleBorder);
-    sketch.glcolor(circleBorder);
-    sketch.circle(0.02 + diameter, 0, 360);
+    sketch.glcolor(circleBorder)
+    sketch.circle(0.02 + diameter, 0, 360)
 
     // inner colored circle
     const color = colors[(1152 + tap.note_incr) % colors.length]
-    sketch.glcolor(color);
-    sketch.circle(diameter, 0, 360);
-    sketch.glcolor(COLOR_BG);
+    sketch.glcolor(color)
+    sketch.circle(diameter, 0, 360)
+    sketch.glcolor(COLOR_BG)
     sketch.text(tap.fib.toString())
     //utils.log(tap.fib.toString())
   }
-  sketch.glcolor(COLOR_LINE);
+  sketch.glcolor(COLOR_LINE)
 
   if (uiPattern.length > 0) {
-    sketch.textalign("center", "center");
-    sketch.glcolor(COLOR_TITLE);
-    outlet(OUTLET_DUR, Math.floor(uiPattern[uiPattern.length - 1].time_offset));
+    sketch.textalign('center', 'center')
+    sketch.glcolor(COLOR_TITLE)
+    outlet(OUTLET_DUR, Math.floor(uiPattern[uiPattern.length - 1].time_offset))
   }
 }
 
 function update() {
-  uiPattern = arrayfromargs(arguments); // magical M4L js function
+  uiPattern = arrayfromargs(arguments) // magical M4L js function
   //utils.log("HEAD: " + JSON.stringify(uiPattern[0]));
   //utils.log("Received: " + JSON.stringify(uiPattern));
-  draw();
-  refresh();
+  draw()
+  refresh()
 }
--- src/fibonacciNoteEcho.ts
+++ src/fibonacciNoteEcho.ts
@@ -2,65 +2,60 @@ inlets = 9
 outlets = 4
 
 function log(_: any) {
-  for (var i = 0, len = arguments.length; i < len; i++) {
-    var message = arguments[i];
+  for (let i = 0, len = arguments.length; i < len; i++) {
+    const message = arguments[i]
     if (message && message.toString) {
-      var s = message.toString();
-      if (s.indexOf("[object ") >= 0) {
-        s = JSON.stringify(message);
+      let s = message.toString()
+      if (s.indexOf('[object ') >= 0) {
+        s = JSON.stringify(message)
       }
-      post(s);
-    }
-    else if (message === null) {
-      post("<null>");
-    }
-    else {
-      post(message);
+      post(s)
+    } else if (message === null) {
+      post('<null>')
+    } else {
+      post(message)
     }
   }
-  post("\n");
+  post('\n')
 }
 
-// inlet index -- used to identify an element in the 
+// inlet index -- used to identify an element in the
 // 'options' array defined below
-const INLET_NOTE = 0;
-const INLET_VELOCITY = 1;
-const INLET_TIME_BASE = 2;
-const INLET_TIMESCALE = 3;
-const INLET_ITERATIONS = 4;
+const INLET_NOTE = 0
+const INLET_VELOCITY = 1
+const INLET_TIME_BASE = 2
+const INLET_TIMESCALE = 3
+const INLET_ITERATIONS = 4
 const INLET_DUR_BASE = 5
-const INLET_DUR_DECAY = 6;
-const INLET_NOTE_INCR = 7;
-const INLET_VELOCITY_DECAY = 8;
+const INLET_DUR_DECAY = 6
+const INLET_NOTE_INCR = 7
+const INLET_VELOCITY_DECAY = 8
 
 // the position in the options array corresponds to the inlet index
 const options = [
-  0,     // INLET_NOTE
-  0,     // INLET_VELOCITY
-  300,   // INLET_TIME_BASE
-  1,     // INLET_TIMESCALE
-  4,     // INLET_ITERATIONS
-  250,   // INLET_DUR_BASE
+  0, // INLET_NOTE
+  0, // INLET_VELOCITY
+  300, // INLET_TIME_BASE
+  1, // INLET_TIMESCALE
+  4, // INLET_ITERATIONS
+  250, // INLET_DUR_BASE
   0.667, // INLET_DUR_DECAY
-  0,     // INLET_NOTE_INCR
-  0.8    // INLET_VELOCITY_DECAY
-];
+  0, // INLET_NOTE_INCR
+  0.8, // INLET_VELOCITY_DECAY
+]
 
 // outlets
-const OUTLET_NOTE = 0;
-const OUTLET_VELOCITY = 1;
-const OUTLET_DURATION = 2;
-const OUTLET_JSUI = 3;
+const OUTLET_NOTE = 0
+const OUTLET_VELOCITY = 1
+const OUTLET_DURATION = 2
+const OUTLET_JSUI = 3
 
-
-let pattern: Step[] = [];
-
-//setupPattern();
+let pattern: Step[] = []
 
 // Method to calculate the Fibonacci pattern for the current knob values.
 function setupPattern() {
   //log(options);
-  pattern = [];
+  pattern = []
 
   // first note plays immediately
   pattern.push({
@@ -68,30 +63,32 @@ function setupPattern() {
     fib: 0,
     velocity_coeff: 1,
     duration: options[INLET_DUR_BASE],
-    time_offset: 0
-  });
+    time_offset: 0,
+  })
 
-  var prv = 1;
-  var fib = 1;
-  var tmp;
-  var prv_time_offset = 0;
-  var new_time_offset = 0;
+  let prv = 1
+  let fib = 1
+  let tmp
+  let prv_time_offset = 0
+  let new_time_offset = 0
 
-  for (var i = 1; i < options[INLET_ITERATIONS]; i++) {
+  for (let i = 1; i < options[INLET_ITERATIONS]; i++) {
     //log(fib);
-    new_time_offset = prv_time_offset + options[INLET_TIMESCALE] * options[INLET_TIME_BASE] * fib
+    new_time_offset =
+      prv_time_offset +
+      options[INLET_TIMESCALE] * options[INLET_TIME_BASE] * fib
 
     pattern.push({
       note_incr: i * options[INLET_NOTE_INCR],
       fib,
       velocity_coeff: Math.pow(options[INLET_VELOCITY_DECAY], i),
       duration: options[INLET_DUR_BASE] * Math.pow(options[INLET_DUR_DECAY], i),
-      time_offset: new_time_offset
-    });
-    tmp = fib;
-    fib = fib + prv;
-    prv = tmp;
-    prv_time_offset = new_time_offset;
+      time_offset: new_time_offset,
+    })
+    tmp = fib
+    fib = fib + prv
+    prv = tmp
+    prv_time_offset = new_time_offset
   }
   //log(pattern);
 
@@ -99,17 +96,16 @@ function setupPattern() {
   // 'update' method in the jsui object with the rest of the pattern array as
   // js args. This results in the visualization being redrawn.
   const updateCmd = 'update' as string | Step
-  outlet(OUTLET_JSUI, [updateCmd].concat(pattern));
+  outlet(OUTLET_JSUI, [updateCmd].concat(pattern))
 }
-
 
 // Returns a function that when executed will send a note of a given pitch,
 // velocity, and duration to the outlets.
 function makeTask(i: number, p: Step, n: number, v: number) {
   return function () {
-    n = n + p.note_incr;
-    v = Math.floor(v * p.velocity_coeff);
-    var d = p.duration
+    n = n + p.note_incr
+    v = Math.floor(v * p.velocity_coeff)
+    const d = p.duration
 
     //log({
     //  i: i,
@@ -118,42 +114,49 @@ function makeTask(i: number, p: Step, n: number, v: number) {
     //  d: d,
     //});
 
-    outlet(OUTLET_JSUI, ["flash"].concat(i.toString()));
-    outlet(OUTLET_DURATION, d);
-    outlet(OUTLET_VELOCITY, v);
-    outlet(OUTLET_NOTE, n);
+    outlet(OUTLET_JSUI, ['flash'].concat(i.toString()))
+    outlet(OUTLET_DURATION, d)
+    outlet(OUTLET_VELOCITY, v)
+    outlet(OUTLET_NOTE, n)
   }
 }
 
 function msg_int(value: number) {
   // integer value received
-  handleMessage(value);
+  handleMessage(value)
 }
 function msg_float(value: number) {
   // float value received
-  handleMessage(value);
+  handleMessage(value)
 }
 
 // called by msg_* methods above when any input is received, e.g. when a knob value changes
 function handleMessage(value: number) {
   // 'inlet' is set by M4L and corresponds to the inlet number the last message
   // was received on.
-  options[inlet] = value;
+  options[inlet] = value
 
   // The first two inlets are INLET_NOTE and INLET_VELOCITY, so we do not need to recalculate
   // the pattern when a message is received on one of those ... only for higher numbered inlets.
   if (inlet > INLET_VELOCITY) {
-    setupPattern();
+    setupPattern()
   }
 
   if (inlet === INLET_NOTE && options[INLET_VELOCITY] > 0) {
     // note received
-    for (var idx = 0; idx < pattern.length; idx++) {
+    for (let idx = 0; idx < pattern.length; idx++) {
       // Schedule a note-playing task to execute for each element in the
       // pattern, at time_offset in the future.
       // The first element is time_offset === 0.
-      var t = new Task(makeTask(idx, pattern[idx], options[INLET_NOTE], options[INLET_VELOCITY]));
-      t.schedule(pattern[idx].time_offset);
+      const t = new Task(
+        makeTask(
+          idx,
+          pattern[idx],
+          options[INLET_NOTE],
+          options[INLET_VELOCITY]
+        )
+      )
+      t.schedule(pattern[idx].time_offset)
     }
   }
 }
